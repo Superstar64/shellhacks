17a151803a2ce2f35c4523d953e08af059e2aff0
Mattv78 <160672579+Mattv78@users.noreply.github.com>
2024-09-28 19:46:42 -0400

    Added Descriptive Label and Pharmacy Tab

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QPushButton
+from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QVBoxLayout, QLabel
 import sys
 
 app = QApplication(sys.argv)
@@ -6,7 +6,17 @@ app = QApplication(sys.argv)
 window = QTabWidget()
 
 addDrug = QWidget()
+vBox = QVBoxLayout()
+intro = QLabel("This is a Program for logging newly created drugs")
+vBox.addWidget(intro)
+
+
+addDrug.setLayout(vBox)
 window.addTab(addDrug, 'Add Drug')
+
+
+registerPharmacy = QWidget()
+window.addTab(registerPharmacy, "Register Pharmacy")
 
 window.show()
 
