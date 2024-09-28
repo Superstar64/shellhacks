78783a6089839e2a18ef538eac456bab1dc9e4ad
Freddy Cubas <hexagonalstar64@gmail.com>
2024-09-28 18:50:26 -0400

    added default.sql

---
+++ default.sql
@@ -0,0 +1,2 @@
+create table drugs(id integer primary key autoincrement, name text not null);
+create table pharmacies(id integer primary key autoincrement, name text not null, address text not null);
