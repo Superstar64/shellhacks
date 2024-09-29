77ddddd3a0f8899179070d75b2371d99db1b061f
Freddy Cubas <hexagonalstar64@gmail.com>
2024-09-28 20:12:18 -0400

    Load database on startup

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from PySide6.QtWidgets import QApplication, QWidget, QTabWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QTabWidget,
+    QPushButton,
+    QFileDialog,
+    QMessageBox,
+    QLabel,
+    QVBoxLayout
+)
+from sqlite3 import (
+    connect
+)
 import sys
 
 app = QApplication(sys.argv)
 
-window = QTabWidget()
+picker = QFileDialog(caption='Select Database',filter='Databases (*.sqlite)')
+if picker.exec():
+    fileNames = picker.selectedFiles()
+    if len(fileNames) > 1:
+        QMessageBox.critical('Too many database selected')
+        sys.exit(1)
+    fileName = fileNames[0]
+    database = connect(fileName)
+    cursor = database.cursor()
 
-addDrug = QWidget()
-vBox = QVBoxLayout()
-intro = QLabel("This is a Program for logging newly created drugs")
-vBox.addWidget(intro)
+    window = QTabWidget()
+
+    addDrug = QWidget()
+    vBox = QVBoxLayout()
+    intro = QLabel("This is a Program for logging newly created drugs")
+    vBox.addWidget(intro)
 
 
-addDrug.setLayout(vBox)
-window.addTab(addDrug, 'Add Drug')
+    addDrug.setLayout(vBox)
+    window.addTab(addDrug, 'Add Drug')
 
 
-registerPharmacy = QWidget()
-window.addTab(registerPharmacy, "Register Pharmacy")
+    registerPharmacy = QWidget()
+    window.addTab(registerPharmacy, "Register Pharmacy")
 
-window.show()
+    window.show()
 
-sys.exit(app.exec())
+    sys.exit(app.exec())
