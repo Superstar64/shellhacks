5acc0df88613e04229af17ddc4bd467907919d20
Mattv78 <160672579+Mattv78@users.noreply.github.com>
2024-09-29 00:01:54 -0400

    Added 'Walgreens' pharmacies

--- default.sql
+++ default.sql
@@ -25,4 +25,7 @@ insert into drug_type (name) values
 insert into pharmacies (name, address) values
     ('CVS', '12701 SW 42nd St, Miami, FL 33175'),
     ('CVS', '9031 SW 107th Ave, Miami, FL 33176'),
-    ('CVS', '10660 SW 40th St, Miami, FL 33165');
+    ('CVS', '10660 SW 40th St, Miami, FL 33165'),
+    ('Walgreens', '1601 SW 107th Ave, Miami, FL 33165'),
+    ('Walgreens', '10700 W Flagler St, Miami, FL 33174'),
+    ('Walgreens', '4010 SW 137th Ave, Miami, FL 33175');
