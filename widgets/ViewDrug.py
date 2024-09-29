be96068e42b46acb0376374382a06fa0c675b0a4
Superstar64 <Hexagonalstar64@gmail.com>
2024-09-29 01:54:00 -0400

    added titles to view drug table

--- widgets/ViewDrug.py
+++ widgets/ViewDrug.py
@@ -13,15 +13,18 @@ class ViewDrug(QWidget):
 
     def update(self):
         data = []
-        for (id, drug, pharamacy) in self.database.execute('select drug_id, drug_type.name, pharmacies.name from inventory, drug_type, pharmacies where drug_type.id = drug_type_id and pharmacies.id = pharmacy_id'):
-            data.append ((id, drug, pharamacy))
+        for item in self.database.execute('select drug_id, drug_type.name, pharmacies.name, pharmacies.address from inventory, drug_type, pharmacies where drug_type.id = drug_type_id and pharmacies.id = pharmacy_id'):
+            data.append(item)
         
-        table = QTableWidget(len(data), 3)
+        table = QTableWidget(len(data), 4)
+        table.setColumnWidth(3, 300)
+        table.setHorizontalHeaderLabels(['Drug Id', 'Drug', 'Pharmacy', 'Address'])
         index = 0
-        for (id, drug, pharamacy) in data:
+        for (id, drug, pharamacy, address) in data:
             table.setItem(index, 0, QTableWidgetItem(str(id)))
             table.setItem(index, 1, QTableWidgetItem(str(drug)))
             table.setItem(index, 2, QTableWidgetItem(str(pharamacy)))
+            table.setItem(index, 3, QTableWidgetItem(str(address)))
             index += 1
 
         box = QVBoxLayout()
