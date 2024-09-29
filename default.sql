03b64405d25b5e0d7a48132df8b438aff9332b89
Superstar64 <Hexagonalstar64@gmail.com>
2024-09-28 22:32:16 -0400

    Added inventory to default sql and rename drug to drug type

--- default.sql
+++ default.sql
@@ -1,2 +1,16 @@
-create table drugs(id integer primary key autoincrement, name text not null);
-create table pharmacies(id integer primary key autoincrement, name text not null, address text not null);
+create table drug_type(
+    id integer primary key autoincrement,
+    name text not null
+);
+create table pharmacies(
+    id integer primary key autoincrement,
+    name text not null,
+    address text not null
+);
+create table inventory(
+    drug_id integer primary key autoincrement,
+    drug_type_id integer,
+    pharmacy_id integer,
+    foreign key (drug_type_id) references drug_type(id),
+    foreign key (pharmacy_id) references pharmacies(id)
+);
